f10d3637ac5db061d9f9d80fcb82c47309701bb1
mstuttgart <michellstut@gmail.com>
2016-03-21 14:34:08 -0300

    [NEW] Projeto lista de compras

---
+++ ListaCompras/app/src/main/java/android_mini_projects/mstuttgart/github/com/br/listacompras/MainActivity.java
@@ -0,0 +1,13 @@
+package android_mini_projects.mstuttgart.github.com.br.listacompras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
