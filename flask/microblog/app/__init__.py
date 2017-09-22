7216d4b6487d3fb22fc5ac355e5f11b3576406cb
Michell Stuttgart <michellstut@gmail.com>
2017-09-22 08:26:36 -0300

    [NEW] Adicionado templates e tela de login no microblog

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 # No final para evitar dependencia circular
 from app import views
--- flask/microblog/app/views.py
+++ flask/microblog/app/views.py
@@ -1,10 +1,38 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
-@app.route('/index.html')
+@app.route('/index')
 def index():
     title = 'Testando microblog'
     user = {'nickname': 'Michell'}
+    posts = [
+        {
+            'author': {'nickname': 'Michell'},
+            'body': 'Beaultiful Site',
+        },
+        {
+            'author': {'nickname': 'Michell'},
+            'body': 'Beaultiful Site 2',
+        },
+    ]
 
-    return render_template('index.html', title=title, user=user)
+    return render_template('index.html', title=title, user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+
+        return redirect('/index')
+
+    else:
+        return render_template('login.html',
+                               title='Sign In',
+                               form=form,
+                               providers=app.config['OPENID_PROVIDERS'])
