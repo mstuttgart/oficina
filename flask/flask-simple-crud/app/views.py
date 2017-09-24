2f6107320cc7087c9bbe412b7722d722da60e6ff
Michell Stuttgart <michellstut@gmail.com>
2017-09-23 22:51:57 -0300

    [NEW][FIX] Template e metodo add funcionando

--- flask/flask-simple-crud/app/views.py
+++ flask/flask-simple-crud/app/views.py
@@ -7,14 +7,28 @@ def index():
     books = models.Book.query.all()
     return render_template('index.html', books=books)
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/add-book', methods=['GET', 'POST'])
 def add_book():
-
     if request.method == 'POST':
         book = models.Book()
         book.title = request.form['title']
+        book.year = request.form['year']
+        book.description = request.form['description']
         db.session.add(book)
         db.session.commit()
         return redirect('/')
     else:
         return render_template('add-book.html')
+
+@app.route('/<int:book_id>', methods=['GET', 'POST'])
+def update_book(book_id):
+    book = models.Book.get(book_id)
+
+    if request.method == 'POST':
+        book.title = request.form['title']
+        book.year = request.form['year']
+        book.description = request.form['description']
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('update-book.html')
--- flask/flask-simple-crud/config.py
+++ flask/flask-simple-crud/config.py
@@ -1,5 +1,6 @@
 import os
 
+DEBUG = True
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
