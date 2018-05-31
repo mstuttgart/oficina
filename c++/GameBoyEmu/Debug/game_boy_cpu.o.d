9286dbb4b39422358c7b252a0d2272c45f611fa6
Michell Stuttgart <michellstut@gmail.com>
2018-05-31 01:26:26 -0300

    [DEL] Remove arquivos de compilação

--- c++/GameBoyEmu/Debug/game_boy_cpu.o.d
+++
@@ -1,5 +0,0 @@
-Debug/game_boy_cpu.o: game_boy_cpu.cpp game_boy_cpu.hpp map_memory.hpp
-
-game_boy_cpu.hpp:
-
-map_memory.hpp:
--- c++/GameBoyEmu/Release/game_boy_cpu.cpp.o.d
+++
@@ -1,6 +0,0 @@
-Release/game_boy_cpu.cpp.o: game_boy_cpu.cpp game_boy_cpu.hpp \
- map_memory.hpp
-
-game_boy_cpu.hpp:
-
-map_memory.hpp:
