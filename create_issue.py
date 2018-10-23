51506c3291092f0d08da13fc963f234c8b603cdb
Michell Stuttgart <michellstut@gmail.com>
2018-10-23 09:31:34 -0300

    [IMP] Adiciona criação de issue adicional para caso de livros

--- create_issue.py
+++ create_issue.py
@@ -5,12 +5,12 @@ USERNAME = 'mstuttgart'
 PASSWORD = ''
 REPO = 'mstuttgart/plano-de-estudos'
 
-MILESTONE_TITLE = 'Milestone Name'
-ISSUE_TITLE = 'Issue Name - 0%d'
+MILESTONE_TITLE = 'Titulo milestone'
 
-LABELS = ['Category: Courses', 'Category: Idea']
+ISSUE_TITLE = 'Titulo - 0%d'
+ISSUE_QUANT = 1
+ISSUE_LABELS = ['Category: Books', 'Category: Idea']
 
-ISSUE_QUANT = 3
 
 # First create a Github instance:
 g = Github(USERNAME, PASSWORD)
@@ -19,11 +19,34 @@ g = Github(USERNAME, PASSWORD)
 repo = g.get_repo(REPO)
 
 # Get labels object to list
-labels = [repo.get_label(label) for label in LABELS]
+labels = [repo.get_label(label) for label in ISSUE_LABELS]
 
 # Create a milestone
 milestone = repo.create_milestone(MILESTONE_TITLE)
 
+comment = ''
+
 # Create a issue set
 for i in range(ISSUE_QUANT):
-    repo.create_issue(title=ISSUE_TITLE % (i + 1), milestone=milestone, labels=labels, assignee=USERNAME)
+    issue = repo.create_issue(
+        title=ISSUE_TITLE % (i + 1),
+        milestone=milestone,
+        labels=labels,
+        assignee=USERNAME,
+    )
+    comment += '#' + issue.id + "\n"
+
+# Case estejamos criando issues para livros
+# Criamos uma issue adicional para anexar a
+# milestone de 12 livros em um ano
+if 'Category: Books' in labels:
+    # Create a milestone
+    milestone = repo.get_milestone(1)
+    repo.create_issue(
+        title=MILESTONE_TITLE,
+        body=comment,
+        milestone=milestone,
+        labels=labels,
+        assignee=USERNAME,
+    )
+
