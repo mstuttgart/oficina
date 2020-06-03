4a3b5ac12e5429d67b1463131ad3cfe625440c2c
Michell Stuttgart <michellstut@gmail.com>
2020-06-03 09:01:25 -0300

    Curso Godot

--- godot/Action RPG/Player/Player.gd
+++ godot/Action RPG/Player/Player.gd
@@ -1,16 +1,33 @@
 extends KinematicBody2D
 
+const ACELLERATION = 500
+const MAX_SPEED = 80
+const FRICTION = 500
+
 var velocity = Vector2.ZERO
-	
+
+# Variavel criada quando a func 'ready' e executada
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get('parameters/playback')
+
 func _physics_process(delta):
 	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	if input_vector != Vector2.ZERO:
-		velocity = input_vector
-	else:
-		velocity = Vector2.ZERO
+	input_vector = input_vector.normalized()
 
-	move_and_collide(velocity)
+	if input_vector != Vector2.ZERO:
+		animationTree.set('parameters/Idle/blend_position', input_vector)
+		animationTree.set('parameters/Run/blend_position', input_vector)
+		animationState.travel('Run')
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACELLERATION * delta)
+		
+	else:
+		animationState.travel('Idle')
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+
+	velocity = move_and_slide(velocity)
