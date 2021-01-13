01e000129a1d87fb4af56f232342890d83ba5e20
Michell Stuttgart <michellstut@gmail.com>
2021-01-13 20:42:30 -0300

    Adiciona Mini Plataform Game - Parte #1
    Tutorial Playlist: https://www.youtube.com/playlist?list=PL9FzW-m48fn2jlBu_0DRh7PvAt-GULEmd

---
+++ godot/Mini Platform Game/KinematicBody2D.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -600
+
+var motion = Vector2()
+ 
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
+		
+	if is_on_floor():
+		
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		
+	motion = move_and_slide(motion, UP)
