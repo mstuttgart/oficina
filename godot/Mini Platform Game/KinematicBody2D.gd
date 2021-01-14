bab005286f76630cfb0b014421de840ffe7e46bc
Michell Stuttgart <michellstut@gmail.com>
2021-01-14 20:12:20 -0300

    Adiciona Mini Plataform Game - Parte #3

--- godot/Mini Platform Game/KinematicBody2D.gd
+++ godot/Mini Platform Game/KinematicBody2D.gd
@@ -2,8 +2,9 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
-const JUMP_HEIGHT = -600
+const ACCELERATION = 50
+const MAX_SPEED = 200
+const JUMP_HEIGHT = -500
 
 var motion = Vector2()
  
@@ -11,25 +12,43 @@ func _physics_process(delta):
 	
 	motion.y += GRAVITY
 	
+	var friction = false
+	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		
+		# Limite horizontal speed
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 		
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
-		motion.x = 0
+		friction = true
+		# Interpolacao para desascelerar personagem simulando fricçao
+		motion.x = lerp(motion.x, 0, 0.2)
 		$Sprite.play("Idle")
-		 
+		
 	if is_on_floor():
 		
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
 			
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
+
 	else:
-		$Sprite.play("Jump")
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 		
 	motion = move_and_slide(motion, UP)
